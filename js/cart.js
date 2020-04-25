433b4c9e08f7e46899244acc5a112e5631eddf8c
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-04-26 04:05:06 +0530

    new pages

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+var but = document.querySelectorAll("#remove a");
+
+
+
+/*
+var len = but.length;
+
+for (var i = 0; i < but.length; i++) {
+    but[i].addEventListener('click', function () {
+        var rem = but[i].parentElement.parentElement.parentElement.parentElement
+        rem.remove();
+        var but = document.querySelectorAll("#remove a");
+        len = len-1;
+    });
+}
+
