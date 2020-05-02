f70e403bb8a36f97fbdee152e047f5b905ab4dec
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-05-02 22:41:37 +0530

    checkuser.js

---
+++ js/checkuser.js
@@ -0,0 +1,4 @@
+window.onload = function checklogin() {
+    document.getElementById("nav-sign-up").style.display = "none";
+    document.getElementById("nav-user-icon").href = "/teacher-account.html";
+  }
