f35a5792c651af54f67eb35bdf272ad75d76a33e
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-05-04 13:02:16 +0530

    user icon fixed

--- js/checkuser.js
+++ js/checkuser.js
@@ -10,7 +10,7 @@ window.onload = async function checklogin() {
         document.getElementById("nav-user-icon").style.display = "block";
         if(localStorage.getItem("acc_type") == 'professor' ) {
             url = '/teacher-account.html'
-        } else {
+        } else if(localStorage.getItem("acc_type") == 'student' ) {
             url = '/my-account.html'
         }
 
